3cfe764d3a9262315b2d4be05bd1d6e0fd328fc5
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-03-10 10:28:24 -0300

    Variáveis

---
+++ variaveis/script.js
@@ -0,0 +1,47 @@
+var nome = 'Julio';
+var idade = 23;
+var possuiFavuldade = true;
+console.log(nome,idade, possuiFavuldade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+
+var sobrenome = 'Rafael', 
+    cidade = 'Rio';
+console.log(sobrenome,cidade)
+
+var semDefinir;
+console.log(semDefinir);
+// console.log(aindaNaoDefinir);
+
+var comida;
+console.log(comida);
+comida = "PIZZA";
+console.log(comida);
+
+var time1 = 'Nautico';
+var time1 = 'Sport';
+console.log(time1);
+
+let time2 = 'Nautico';
+time2 = 'Sport';
+console.log(time2);
+
+// Exercicio
+
+var nome2 = 'Julio';
+var idade2 = 23;
+var comidaFavorita;
+
+comidaFavorita = 'Yakizoba';
+console.log(comidaFavorita,nome2,idade2);
+var esporte;
+var anime;
+var serie;
+var filme;
+var lugar;
+
+
+
