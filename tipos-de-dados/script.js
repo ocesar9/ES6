907009d4a5e20cd2721e895d45e05946b398dcf5
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-03-10 10:52:17 -0300

    Tipos de Dados

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,46 @@
+var nome = 'Andre';
+var idade = 20;
+var time = null;
+console.log(typeof idade,typeof nome,typeof time);
+
+var nome2 = 'Julio';
+var sobrenome = 'Guimaraes';
+
+var nomeCompleto = nome2 + ' ' + sobrenome;
+console.log(nomeCompleto)
+
+var gols = 1000;
+var frase = 'Ronaldo fez ' + gols + " gols";
+console.log(frase);
+console.log(typeof frase)
+
+var ano = '2022 ';
+var mes = 12;
+console.log(ano + mes);
+
+var melhor = 'teste'
+var frase2 = "Esse e o \" melhor\" jogo";
+console.log(frase2);
+
+var gols2 = 1000;
+var frase3 = `Ronaldo fez ${gols*2} gols`
+console.log(frase3);
+
+// Exercicio
+
+var bebida = 'cachaca';
+var bebidaLitros = '23';
+console.log(typeof bebidaLitros,typeof bebida);
+
+var idade = 23
+console.log(idade);
+
+var nome2 = 'Julio';
+var sobrenome2 = 'Guimaraes';
+var nomeCompleto2 = `${nome2} ${sobrenome2}` ;
+console.log(nomeCompleto2);
+
+var frase4 = `It's time`;
+
+console.log(typeof nome2);
+
