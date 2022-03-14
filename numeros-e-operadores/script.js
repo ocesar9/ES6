7a93d524afe49872be9a081a4af8487fd0e1b869
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-03-14 10:13:55 -0300

    Números e Operadores 1

---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,21 @@
+var idade = 23;
+var decimalPositivo = 2e3;
+var decimalNegativo = 2e-3;
+console.log(idade);
+console.log(decimalPositivo, decimalNegativo);
+
+var exponencial =  2 ** 4;
+console.log(exponencial);
+
+var total = 10 + 5 + 9;
+var total2 = 200/5;
+var modulo = 332453 /3;
+
+console.log(total, total2,modulo);
+
+var soma = "10" + 10;
+var divisao = "10" /5;
+var soma2 = 'Isso 100' /2;
+console.log(soma, divisao, soma2);
+console.log(isNaN(divisao));
+
