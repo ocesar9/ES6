2b341f6061b9b1439c887e9ad11ad1b1c1da12c7
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-03-14 10:51:15 -0300

    Números e Operadores 2

--- numeros-e-operadores/script.js
+++ numeros-e-operadores/script.js
@@ -19,3 +19,46 @@ var soma2 = 'Isso 100' /2;
 console.log(soma, divisao, soma2);
 console.log(isNaN(divisao));
 
+var soma1 = 10+10+20*40/(2+10);
+console.log(soma1);
+
+var imcremento = 5;
+imcremento = imcremento + 1;
+console.log(imcremento);
+console.log(imcremento++);
+console.log(imcremento);
+
+var decremento = 5;
+decremento = decremento - 1;
+console.log(decremento);
+console.log(decremento--);
+console.log(decremento);
+
+var idade2 = '20';
+var somaIdade = -5;
+console.log(+idade2 + somaIdade);
+
+// Exercicio
+var total1 = 10 + 5 + 2/2 + 20;
+console.log(total1);
+
+var expressao1 = 'Isso nao e uma soma' / 20;
+var expressao2 = 'Isso nao e uma soma' ** 20;
+console.log(expressao1,expressao2);
+
+var string = '200';
+var numero = 50;
+console.log(+string + numero);
+
+var imcremento2 = 5;
+imcremento2++;
+console.log(imcremento2);
+
+var numero2 = +'90'/2;
+var unidade = 'kg';
+var pesoPorDois = numero2 + unidade;
+console.log(pesoPorDois);
+
+
+
+
