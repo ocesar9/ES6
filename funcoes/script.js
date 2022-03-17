918bcade2414666aa0afdeceda66e9ad6be28d1c
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-03-16 22:48:40 -0300

    Funções 2

--- funcoes/script.js
+++ funcoes/script.js
@@ -39,3 +39,62 @@ function mostraConsole(){
 }
 
 addEventListener('click', mostraConsole);
+
+function imc2(peso, altura){
+    const imc = peso / (altura ** 2);
+    console.log(imc);
+}
+
+imc2(80,1.9);
+console.log(imc2(80,1.9))
+
+function terceiraIdade(idade){
+    console.log(idade);
+    console.log(typeof(idade));
+    if(typeof(idade !== 'number')){
+        return 'Por favor preencha um numero!'
+    }
+    if(idade >= 60){
+        console.log('E idoso!');
+        return true;
+    }else {
+        return false;
+    }
+}
+console.log(terceiraIdade(30));
+console.log(terceiraIdade("30"));
+
+var totalPaises = 193;
+function faltaVisitar(paisesVisitados){
+        return `Faltam visitar ${totalPaises - paisesVisitados} paises`
+}
+console.log(faltaVisitar(3));
+console.log(totalPaises)
+
+var profissao = 'Designer';
+function dados() {
+    var nome = 'Julio';
+    var idade = 23;
+    function outrosDados() {
+        var endereco = 'Pernambuco';
+        var idade = 23;
+        return `${nome}, ${idade}, ${endereco}, ${profissao}`;
+    }
+    return outrosDados();
+}
+
+console.log(dados())
+
+
+imc3(100,1.9);
+function imc3(peso, altura){
+    const imc = peso / (altura ** 2);
+    console.log(imc);
+}
+
+
+
+
+
+
+
