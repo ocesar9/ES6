d17cfa701b9f7760541f1feca9e4364bbbe8f905
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-03-21 10:15:44 -0300

    Objetos 2

--- objetos/script.js
+++ objetos/script.js
@@ -25,5 +25,74 @@ console.log(quadrado.cinco());
 console.log(Math.PI)
 console.log(Math.random())
 
+var a = {}
+console.log(typeof a);
+
+var height = 120;
+var menu = {
+  with: 800,
+  height: 50,
+  backgroundColor: '#84e',
+  metadeheight(){
+    return this.height/2;
+  }
+}
+console.log(menu.backgroundColor)
+menu.backgroundColor = '#000000';
+console.log(menu.backgroundColor)
+
+var bg = menu.backgroundColor
+bg = 'blue'
+console.log(menu.backgroundColor)
+
+menu.esconder = function() {
+  console.log('Escondi')
+}
+menu.esconder();
+console.log(menu.metadeheight());
+
+console.log(menu.hasOwnProperty('backgroundColor'))
+
+// Exercicio
+
+var eu = {
+  nome: 'Julio',
+  sobrenome: 'Guimaraes',
+  idade: 23,
+  genero: 'masculino',
+  meunome(){
+    return `${this.nome} ${this.sobrenome}`;
+  }
+}
+console.log(eu.meunome());
+
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marcar: 'Audi'
+}
+
+carro.preco = 3000
+console.log(carro);
+
+var cachorro = {
+  raca: 'labrador',
+  cor: 'preto',
+  idade: 10,
+  late(pessoa){
+    if(pessoa === 'homem'){
+      console.log("Au au!")
+    }else if (pessoa === 'mulher'){
+      console.log('Cara de bobo!')
+    }
+  }
+}
+
+cachorro.late(`homem`);
+cachorro.late(`mulher`);
+
+
+
+
 
 
