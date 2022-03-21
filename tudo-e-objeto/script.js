df724b12480d0944976c4f8337d8a64375b29a8f
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-03-21 15:29:26 -0300

    Tudo é Objeto

---
+++ tudo-e-objeto/script.js
@@ -0,0 +1,57 @@
+var nome = 'Andre';
+
+console.log(nome.length);
+
+console.log(nome.toLowerCase());
+
+var numero = 11.8;
+console.log(numero)
+numero = numero.toFixed()
+console.log(numero)
+
+console.log(addEventListener.length);
+
+var btn = document.querySelector('.btn');
+btn.addEventListener('click', function() {
+    event.preventDefault();
+    console.log('Clique')
+})
+btn.classList.add('ativo')
+
+console.log(btn)
+
+// Exercicio
+
+var nome = 'Apellido';
+// fixed
+// lenght
+// slice
+
+
+var btn = document.querySelector('.btn');
+
+// addEventListener
+// appendChild
+// id
+// innerHTML
+// outerHTML
+
+// Clipboard
+
+// Cut text from another element
+
+// <!-- Target -->
+// <textarea id="bar">Mussum ipsum cacilds...</textarea>
+
+// <!-- Trigger -->
+// <button class="btn" data-clipboard-action="cut" data-clipboard-target="#bar">
+//     Cut to clipboard
+// </button>
+
+
+// Copy text from attribute
+
+// <!-- Trigger -->
+// <button class="btn" data-clipboard-text="Just because you can doesn't mean you should — clipboard.js">
+//     Copy to clipboard
+// </button>
