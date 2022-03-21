74de3e2ca038e3fa2935538ac5775e960f5f781a
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-03-21 16:12:22 -0300

    Arrays e Loops 1

---
+++ arrays-e-loops/script.js
@@ -0,0 +1,26 @@
+var videogames = ['Switch', 'PS4', 'XBOX']
+console.log(videogames);
+
+console.log(videogames[2]);
+
+videogames.pop()
+console.log(videogames.length);
+console.log(videogames);
+
+videogames.push('3DS')
+console.log(videogames);
+
+for (var numero = 1; numero <= 10; numero++){
+    console.log(numero);
+}
+
+var i = 0;
+while(i < 10){
+    console.log(i);
+    i++;
+}
+
+
+
+
+
