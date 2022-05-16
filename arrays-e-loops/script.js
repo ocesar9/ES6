89a100312422afae269182d0e2d9961e41feca0b
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-05-16 09:54:25 -0300

    Arrays e Loops 2

--- arrays-e-loops/script.js
+++ arrays-e-loops/script.js
@@ -20,7 +20,52 @@ while(i < 10){
     i++;
 }
 
+var videogames2 = ['Switch', 'PS4', 'XBOX', '3DS']
 
+for (let item = 0; item < videogames2.length; item++) {
+    console.log(videogames2[item]);
+    if(videogames2[item] === 'XBOX') {
+        break;
+    }
+}
 
+videogames2.forEach(function(item){
+    console.log(item);
+})
 
+var frutas = ['banana', 'pera', 'abacaxi', 'uva']
+
+frutas.forEach(function(fruta,index,array){
+    frutas.pop();
+    console.log(fruta,index,array)
+})
+
+var numero = 0;
+var maximo = 50;
+for(;numero < maximo;){
+    console.log(numero)
+    numero++;
+}
+
+// Exercicio
+
+var titulosBrasilAno = ['1959', '1962', '1970', '1994', '2002']
+
+for(let i = 0; i < titulosBrasilAno.length; i++){
+    console.log(`O brasil ganhou a copa de ${titulosBrasilAno[i]}`)
+}
+
+var frutas2 = ['Banana','Maca', 'Pera', 'Uva', 'Melancia']
+
+for (let i =0;i < frutas2.length; i++){
+    console.log(frutas2[i])
+    if(frutas2[i] === 'Pera'){
+        break;
+    }
+}
+
+var ultimaFruta = frutas2[frutas2.length-1]
+
+console.log(frutas2);
+console.log(ultimaFruta)
 
