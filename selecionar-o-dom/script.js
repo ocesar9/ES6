b521d2a683c19ff5e659b6c85f443ae10e65a9e7
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-07-02 21:53:06 -0300

    Seleção de Elementos 2

--- selecionar-o-dom/script.js
+++ selecionar-o-dom/script.js
@@ -19,3 +19,49 @@ console.log(linksInterno);
 const animaisImg = document.querySelectorAll('.animais img');
 console.log(animaisImg);
 console.log(animaisImg[2]);
+
+const gridSectionHtml = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+console.log(gridSectionHtml);
+console.log(gridSectionNode);
+
+primeiraUl.classList.add('grid-section');
+
+console.log(gridSectionHtml[0]);
+console.log(gridSectionNode[0]);
+
+gridSectionNode.forEach(function(item,index){
+  console.log(item);
+  console.log(index);
+})
+
+const arrayGrid = Array.from(gridSectionHtml);
+
+arrayGrid.forEach(function(item){
+  console.log(item);
+})
+
+console.log(arrayGrid);
+arrayGrid.pop();
+console.log(arrayGrid);
+
+// Exercicio
+
+const images = document.querySelectorAll('img');
+console.log(images);
+
+const imagens = document.querySelectorAll("[src^='img/imagem'");
+console.log(imagens);
+
+const linksInternos = document.querySelectorAll("[href^='#'");
+console.log(linksInternos);
+
+const primeitoh2 = document.querySelector(".animais-descricao h2");
+console.log(primeitoh2);
+
+const ultimoP = document.querySelectorAll("p");
+console.log(ultimoP[ultimoP.length - 1])
+
+
+
