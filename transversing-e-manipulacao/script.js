b22c9864708ca3e1e1873d8e027da9d9abc74c76
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-07-28 11:39:43 -0300

    Traversing e Manipulação 2

--- transversing-e-manipulacao/script.js
+++ transversing-e-manipulacao/script.js
@@ -1,23 +1,75 @@
-const h1 = document.querySelector('h1');
-const animaisDescricao = document.querySelector(".animais-descricao");
-console.log(animaisDescricao.innerHTML)
+// const h1 = document.querySelector('h1');
+// const animaisDescricao = document.querySelector(".animais-descricao");
+// console.log(animaisDescricao.innerHTML)
 
-console.log(animaisDescricao.innerText)
+// console.log(animaisDescricao.innerText)
 
-h1.innerHTML = '<p>Novo Titulo</p>';
-// h1.innerText = "Novo Texto";
+// h1.innerHTML = '<p>Novo Titulo</p>';
+// // h1.innerText = "Novo Texto";
 
-console.log(animaisDescricao.parentElement)
-console.log(animaisDescricao.parentElement.parentElement)
+// console.log(animaisDescricao.parentElement)
+// console.log(animaisDescricao.parentElement.parentElement)
 
-console.log(h1.nextElementSibling);
+// console.log(h1.nextElementSibling);
 
-const animaisLista = document.querySelector('.animais-lista');
+// const animaisLista = document.querySelector('.animais-lista');
 
-console.log(animaisLista.children);
-console.log(animaisLista.children[animaisLista.children.length-1]);
+// console.log(animaisLista.children);
+// console.log(animaisLista.children[animaisLista.children.length-1]);
 
-console.log(animaisLista.querySelector('li:last-child'));
+// console.log(animaisLista.querySelector('li:last-child'));
 
-console.log(animaisLista.previousElementSibling)
+// console.log(animaisLista.previousElementSibling)
+
+// const animais = document.querySelector(".animais")
+// const contato = document.querySelector('.contato');
+// const tituloContato = contato.querySelector('.titulo');
+
+// // animaisLista.appendChild(tituloContato);
+
+// const mapa = document.querySelector('.mapa');
+
+// contato.insertBefore(animais,mapa);
+
+// contato.replaceChild(mapa,tituloContato)
+
+// const animaisUltimoElemento = animaisLista.querySelector('li:last-child');
+// animaisLista.removeChild(animaisUltimoElemento);
+
+// const novoh1 = document.createElement('h1'); 
+// novoh1.innerText = "New Title";
+// novoh1.classList.add("titulo");
+// console.log(novoh1);
+
+// mapa.appendChild(novoh1);
+
+  // const h1 = document.querySelector('h1');
+  // const faq = document.querySelector('.faq');
+
+  // faq.appendChild(h1);
+
+  // const cloneH1 = h1.cloneNode(true);
+  // faq.appendChild(cloneH1);
+
+// Exercicio
+
+const menu = document.querySelector('.menu');
+const copy = document.querySelector('.copy');
+
+const cloneMenu = menu.cloneNode(true);
+
+copy.appendChild(cloneMenu);
+
+const faq = document.querySelector('.faq');
+const primeitoDt = faq.querySelector('dt');
+
+console.log(primeitoDt);
+
+const proximoDD = primeitoDt.nextElementSibling;
+console.log(proximoDD.remove())
+
+const animais = document.querySelector('.animais');
+
+faq.innerHTML = animais.innerHTML
+
 
