d8bdbd74311d1b9734ef4b0a09c8420887bc35a5
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-08-16 20:30:26 -0300

    Prototype 2

--- constructor-functions/script.js
+++ constructor-functions/script.js
@@ -26,6 +26,35 @@ console.log(andre.andar())
 console.log(andre.nadar())
 console.log(andre.abracar())
 
+const pais = "Brasil"
+
+const cidade = new String("Rio")
+console.log(cidade)
+
+console.log(pais.charAt(0))
+console.log(cidade.charAt(0))
+
+const listaAnimais = ["cachorro", "gato", "cavalo"]
+
+const lista = document.querySelectorAll("li");
+console.log(lista)
+// Transforma numa array 
+const listaArray1 = Array.prototype.slice.call(lista)
+console.log(listaArray1);
+const listaArray2 = Array.from(lista);
+console.log(listaArray2);
+
+const Carro = {
+  marca: "Ford",
+  preco: 2000,
+  andar(){
+    return true;
+  }
+}
+
+console.log(Carro.preco.constructor.name)
+console.log(Carro.marca.constructor.name)
+console.log(Carro.andar.constructor.name)
 
 
 
