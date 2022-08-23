83149ad22ed316fec54b895adc527ff542baeacf
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-08-22 21:08:11 -0300

    Native, Host e User 2

--- native-host-user /script.js
+++ native-host-user /script.js
@@ -5,3 +5,35 @@ if(typeof Array.fromasdjasdkj !== "undefined") {
 }else {
   console.log("Nao Existe")
 }
+
+// Exercicio
+
+ Object
+ String
+ Array
+ Function
+ Math
+
+
+ window
+ history
+ document
+ HTMLAllCollection
+ NodeList
+
+ if(typeof window.onafterprint !== "undefined") {
+  console.log("Existe!!")
+ }
+
+ if(typeof document.adoptNode !== "undefined") {
+  console.log("Existe!!")
+ }
+
+ if(typeof document.webkitHidden !== "undefined") {
+  console.log("Existe!!")
+ }
+
+ if(typeof document.webkitVisibilityState !== "undefined") {
+  console.log("Existe!!")
+ }
+
