d127c3e1f6f6b9905c052f2e3cb5d35e9e67ed43
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-09-03 21:33:18 -0300

    String 3

--- string/script.js
+++ string/script.js
@@ -71,7 +71,81 @@ console.log(valor.trim());
 console.log(valor.trimStart());
 console.log(valor.trimEnd());
 
-
-
-
 const frutasArray = ["Banna", "Melancia", "Laranja"];
+
+// Exercicio
+
+const transacoes = [
+  {
+    descricao: 'Taxa do Pão',
+    valor: 'R$ 39',
+  },
+  {
+    descricao: 'Taxa do Mercado',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Cliente',
+    valor: 'R$ 99',
+  },
+  {
+    descricao: 'Taxa do Banco',
+    valor: 'R$ 129',
+  },
+  {
+    descricao: 'Recebimento de Taxa Cliente',
+    valor: 'R$ 49',
+  },
+];
+
+let taxaTotal = 0;
+let recebimentoTotal = 0;
+transacoes.forEach(item =>{
+  const numeroLimpo = +item.valor.replace("R$", "");
+  console.log(item.descricao.slice(0,4) === "Taxa");
+  if(item.descricao.slice(0,4) === "Taxa"){
+    taxaTotal += taxaTotal + numeroLimpo;
+  }else if(item.descricao.slice(0,11) === "Recebimento"){
+    recebimentoTotal += recebimentoTotal + numeroLimpo
+  }
+})
+
+console.log(taxaTotal);
+console.log(recebimentoTotal);
+
+
+
+const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+
+const arrayTransportes = transportes.split(';');
+console.log(arrayTransportes);
+
+
+let html = 
+`<ul>
+    <li><span>Sobre</span></li>
+    <li><span>Produtos</span></li>
+    <li><span>Contato</span></li>
+</ul>`;
+
+html = html.split("span").join("a");
+console.log(html);
+
+const frase4 = 'Melhor do ano!';
+
+console.log(frase4.slice(-1));
+
+const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+
+let taxasTotal = 0;
+transacoes2.forEach(item =>{
+  item = item.toLowerCase();
+  item = item.trim();
+  item = item.slice(0,4);
+
+  if(item === "taxa"){
+    taxasTotal++;
+  }
+})
+console.log(taxasTotal);
+
