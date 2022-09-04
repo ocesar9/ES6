51ac7f4ef8c7fbb60fc95e1e425380ea95bfffae
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-09-04 18:54:42 -0300

    Number e Math 1

---
+++ number-e-math/script.js
@@ -0,0 +1,37 @@
+console.log(Number.isNaN(5));
+console.log(Number.isNaN("ds"));
+console.log(Number.isNaN(NaN));
+
+console.log(Number.isInteger(5));
+
+
+console.log(parseFloat("34.3434,23"))
+console.log(parseFloat("100 reais"))
+console.log(parseInt("100.34 reais"))
+
+const preco = 2.34445;
+console.log(preco.toFixed());
+console.log(preco.toString());
+
+
+console.log(preco.toLocaleString("pt-br",{style:'currency',currency: "BRL"}));
+
+
+console.log(Math.PI)
+
+console.log(Math.abs(3-7));
+console.log(Math.ceil(5.1));
+console.log(Math.floor(5.9));
+console.log(Math.round(5.45));
+
+console.log(Math.max(5,45,34,12,50,69,32));
+console.log(Math.min(5,45,34,12,50,69,32));
+console.log(Math.random());
+console.log(Math.random()*100);
+
+console.log(Math.floor(Math.random()*10))
+
+
+
+
+
