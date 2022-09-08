f97b9740bb09d31e68d8da227837e43802b27ccc
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-09-07 21:21:44 -0300

    Array 2

--- array/script.js
+++ array/script.js
@@ -69,13 +69,52 @@ console.log(carroS);
 const novaArray = carroS.push("Parati", "Gol")
 console.log(novaArray);
 
+// Primeiro
 console.log(carroS.pop());
 console.log(carroS);
 
+// Ultimo
 console.log(carroS.shift());
 console.log(carroS);
 
 carroS.reverse()
 console.log(carroS);
+
+// index,remover,item
+console.log(carroS.splice(2,0, "Fusca"))
+
+console.log(carroS);
+
+console.log(carroS.splice(2,1))
+
+console.log(carroS);
+
+// alvo, inicio, final
+console.log(["Item 1","Item 2", "Item 3", "Item 4", "Item 5"].copyWithin(2,0,2))
+
+console.log(["Item 1","Item 2", "Item 3", "Item 4", "Item 5"].copyWithin(-1))
+
+console.log(["Item 1","Item 2", "Item 3", "Item 4", "Item 5"].fill("Banana",2))
+
+const transporte1 = ["Barco", "Aviao"]
+const transporte2 = ["Carro","Moto"]
+
+console.log(transporte1.concat(transporte2));
+console.log(transporte1.concat(transporte2, "Caminhao"));
+
+const linguagens = ["HTML", "JS", "php", "css" , "python"]
+
+console.log(linguagens.includes("css"));
+console.log(linguagens.indexOf("css"));
+
+let htmlString = "<h2>Titulo Principal</h2>";
+htmlString = htmlString.split("h2");
+htmlString = htmlString.join('h1')
+console.log(htmlString);
+
+console.log(linguagens.slice(2,3))
+
+const cloneLinguagens = linguagens.slice();
+console.log(cloneLinguagens)
 
 
