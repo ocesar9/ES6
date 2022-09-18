ffac6e24f0f6cf746bcc40a13eb665035b1d287e
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-09-18 17:17:38 -0300

    Function 2

--- function/script.js
+++ function/script.js
@@ -41,22 +41,27 @@ carros.forEach.call(frutas,(item) => {
   console.log(item);
 })
 
+// Definindo function
 function Dom(selector){
   this.element = document.querySelector(selector)
 }
 
+// Atribuindo nova function
 Dom.prototype.ativo = function(classe) {
   console.log(this)
   this.element.classList.add(classe);
 }
 
+// Declarando function
 const ul = new Dom("ul");
 ul.ativo("ativar")
 
+// Declarando nova function
 const li = {
   element: document.querySelector('li'),
 };
 
+// Atribuindo mais uma function
 Dom.prototype.ativo.call(li,"ativo")
 
 Array.prototype.mostrarThis= function(){
@@ -86,7 +91,38 @@ const filtro = arrayLi.filter((item) => {
   return item.classList.contains("ativo");
 })
 
+const bind = arrayLi.filter.bind((item) => {
+  return item.classList.contains("ativo");
+})
 console.log(filtro)
+console.log(bind)
 console.log(li)
 
+const numeros = [33,323,432,54,5424,4];
+
+console.log(Math.max.apply(null,numeros))
+
+const $ = document.querySelectorAll.bind(document,"li");
+console.log($)
+
+const carro = {
+  marca: "Ford",
+  ano:2018,
+  acelerar:function(aceleracao, tempo){
+    return `${this.marca} acelerou ${aceleracao} em ${tempo}`;
+  }
+}
+
+console.log(carro.acelerar(100,20));
+
+
+const honda = {
+  marca:"Honda",
+
+}
+
+// Atribuindo os parametros pelo bind
+const acelerarHonda = carro.acelerar.bind(honda,100);
+
+console.log(acelerarHonda(20));
 
