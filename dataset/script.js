5d3164860a796f38a91d90be0e2348e716df0481
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-09-25 21:06:23 -0300

    Dataset 1

---
+++ dataset/script.js
@@ -0,0 +1,21 @@
+const h1 = document.querySelector('h1');
+console.log(Object.prototype.toString.call(h1));
+console.log(h1.dataset)
+
+const div = document.querySelector('div');
+console.log(div.dataset)
+console.log(div.dataset.cor)
+
+const dataCor = document.querySelector('[data-cor]');
+console.log(dataCor)
+
+dataCor.dataset.height = 1000;
+console.log(dataCor.dataset)
+
+delete div.dataset.width;
+console.log(dataCor.dataset);
+
+const dataAnimaScroll = document.querySelector('[data-anima-scroll]');
+console.log(dataAnimaScroll.dataset)
+dataAnimaScroll.dataset.totalHeight = 2000
+console.log(dataAnimaScroll.dataset)
