ba6d64f5c84f5377c11d7df23deaec7ae14808a5
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-10-28 11:30:34 -0300

    Modules 1

---
+++ modules-introducao/js/modules/teste.js
@@ -0,0 +1,7 @@
+export function teste1(){
+  console.log("teste 1");
+}
+
+export function teste2(){
+  console.log("teste 2");
+}
---
+++ modules-introducao/js/script.js
@@ -0,0 +1,16 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import {teste1,teste2} from "./modules/teste.js";
+import * as teste from "./modules/teste.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+
+teste1();
+teste2();
+
+teste.teste1();
