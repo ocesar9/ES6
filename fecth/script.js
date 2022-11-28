77bbb5cec499928f619b9404492f39a03088ec4a
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-11-28 11:24:45 -0300

    Fetch 2

--- fecth/script.js
+++ fecth/script.js
@@ -1,4 +1,5 @@
 const doc = fetch('./doc.txt');
+const docError = fetch('./docs.txt');
 const style = fetch('./style.css');
 const cep = fetch('https://viacep.com.br/ws/01001000/json/');
 const conteudo = document.querySelector('.conteudo');
@@ -17,10 +18,19 @@ doc.then(r => {
 console.log(cep);
 
 cep.then(r => {
-  return r.json();
-}).then((body) => {
-  console.log(body.logradouro);
+  const r2 = r.clone();
+  const rText = r.text();
+  rText.then((text) => {
+    console.log(text);
+  })
+
+  const rJson = r2.json();
+  rJson.then((body) => {
+    console.log(body.logradouro);
+  })
 })
+
+  
 
 style.then(r => r.text())
 .then((body) => {
@@ -34,4 +44,33 @@ sobre.then(r => r.text())
   const titulo = div.querySelector('h1');
   document.querySelector('h1').innerText = titulo.innerText;
   console.log(titulo);
+})
+
+const imagem = fetch('./imagem.jpg');
+imagem.then((r) => r.blob())
+.then((body) => {
+  const blobURL = URL.createObjectURL(body);
+  const imagemDom = document.querySelector('img');
+  imagemDom.src = blobURL;
+
+  console.log(body);
+  console.log(blobURL);
+});
+
+
+cep.then((response) => {
+  console.log(response.status);
+  response.headers.forEach(console.log)
+})
+
+const array = ["ITEM 1","ITEM 2","ITEM 3"]
+array.forEach(console.log);
+
+docError.then((response) =>{
+  console.log(response.status);
+  console.log(response.url);
+  console.log(response.type);
+  if(response.status === 404){
+    console.log("Pagina nao existe");
+  }
 })
