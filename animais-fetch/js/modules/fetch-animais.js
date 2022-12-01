5111edf43e0f561a8cbc85fbaca21d3bb9a40852
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-12-01 14:11:21 -0300

    Bitcoin Fetch

--- animais-fetch/js/modules/fetch-animais.js
+++ animais-fetch/js/modules/fetch-animais.js
@@ -2,27 +2,31 @@ import initAnimaNumeros from "./anima-numeros.js";
 
 export default function initFetchAnimais() {
   async function fetchAnimais(url){
-    const animaisResponse = await fetch(url);
-    const animaisJSON = await animaisResponse.json();
-    const numerosGrid = document.querySelector('.numeros-grid');
-    console.log(animaisJSON);
+      try {
+        const animaisResponse = await fetch(url);
+        const animaisJSON = await animaisResponse.json();
+        const numerosGrid = document.querySelector('.numeros-grid');
+        console.log(animaisJSON);
+        
+        animaisJSON.forEach(animal => {
+          const divAnimal = createAnimal(animal);
+          console.log(divAnimal);
+          numerosGrid.appendChild(divAnimal);
+        });
+        initAnimaNumeros();
+      }catch (error) {
+        console.log(Error(error));
     
-    animaisJSON.forEach(animal => {
-      const divAnimal = createAnimal(animal);
-      console.log(divAnimal);
-      numerosGrid.appendChild(divAnimal);
-    });
-    initAnimaNumeros();
+      }
+    } 
+      
+    function createAnimal(animal){
+      const div = document.createElement('div');
+      div.classList.add('numero-animal');
+      div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`
+      return div;
+    }
     
-  }
-  
-  function createAnimal(animal){
-    const div = document.createElement('div');
-    div.classList.add('numero-animal');
-    div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`
-    return div;
-  }
-  
-  fetchAnimais('./animaisapi.json')
+    fetchAnimais('./animaisapi.json')
 }
 
