f2e0080fd74fed43a367e94245bd57e692953451
ocesar9 <juliocesaoguimaraes@gmail.com>
2023-02-08 16:07:57 -0300

    Factory Function 2

--- factory-function/script.js
+++ factory-function/script.js
@@ -48,3 +48,61 @@ console.log(designer1);
 
 const designer2 = new Pessoa('cesar');
 console.log(designer2);
+
+// Exercicio
+
+  function $$(selectedElements){
+    const elements = document.querySelectorAll(selectedElements);
+
+    function hide(){
+      elements.forEach(element => {
+        element.style.display = 'none';
+      })
+      return this
+    }
+
+    function show(){
+      elements.forEach(element => {
+        element.style.display = 'initial';
+      })
+      return $$(selectedElements)
+    }
+
+    function on(onEvent,callback){
+      elements.forEach(element => {
+        element.addEventListener(onEvent, callback)
+      })
+    }
+
+    function addClass(className){
+      elements.forEach(element => {
+        element.classList.add(className)
+      })
+    }
+
+    function removeClass(className){
+      elements.forEach(element => {
+        element.classList.remove(className)
+      })
+    }
+
+    return {
+      elements,
+      hide,
+      show,
+      on,
+      addClass,
+      removeClass
+    }
+  }
+
+const btns = $$('button')
+console.log(btns.hide().show())
+
+function handleClick(event){
+  console.log(event.target)
+  btns.addClass('active')
+}
+
+btns.on('click', handleClick)
+
