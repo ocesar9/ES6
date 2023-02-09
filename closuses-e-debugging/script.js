71eb15597f06c70e4502f622cf030d7ee3dff038
ocesar9 <juliocesaoguimaraes@gmail.com>
2023-02-09 16:08:42 -0300

    Closures e Debugging

---
+++ closuses-e-debugging/script.js
@@ -0,0 +1,28 @@
+let item1 =1;
+
+function funcao1(){
+  let item2 =2;
+  function funcao2(){
+    let item3 =3;
+    console.log(item1);
+    console.log(item2);
+    console.log(item3);
+  }
+  funcao2();
+}
+
+funcao1();
+
+function contagem(){
+  let total = 0;
+  return function incremento(){
+    total++;
+    console.log(total);
+  }
+}
+
+const ativarIncremento = contagem();
+ativarIncremento();
+ativarIncremento();
+ativarIncremento();
+ativarIncremento();
