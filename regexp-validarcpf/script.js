98ef9206abe1b9666078ebf61dc78d41678a159d
ocesar9 <juliocesaoguimaraes@gmail.com>
2023-02-15 09:46:53 -0300

    Validar CPF 2

--- regexp-validarcpf/script.js
+++ regexp-validarcpf/script.js
@@ -1,5 +1,4 @@
 import ValidarCpf from "./validar-cpf.js";
 
 const cpf = document.querySelector('#cpf');
-const validarCpf = new ValidarCpf(cpf);
-console.log(validarCpf.formatar('120 083 994 35'))
+const validarCpf = new ValidarCpf(cpf).iniciar();
--- regexp-validarcpf/validar-cpf.js
+++ regexp-validarcpf/validar-cpf.js
@@ -5,12 +5,39 @@ export default class ValidarCpf{
   limpar(cpf){
     return cpf.replace(/\D/g, '');
   }
-  construir(){
-    return cpf.replace(/\(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
+  construir(cpf){
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
   }
 
   formatar(cpf){
     const cpfLimpo = this.limpar(cpf);
     return this.construir(cpfLimpo);
+  }
+
+  validar(cpf){
+    const matchCpf = cpf.match(/(\d{3}[\s-.]?){3}\d{2}/g);
+    console.log(cpf);
+    console.log(matchCpf);  
+    return (matchCpf && matchCpf[0] === cpf)
+  }
+
+  validarNaMudanca(cpfElement){
+    if(this.validar(cpfElement.value)){
+      cpfElement.value = this.formatar(cpfElement.value);
+    }else{
+      
+    }
+    console.log(this.validar(cpfElement.value));
+  }
+
+  adicionarEvento(){
+    this.element.addEventListener('change', () => {
+      this.validarNaMudanca(this.element);
+    });
+  }
+
+  iniciar(){
+    this.adicionarEvento();
+    return this;
   }
 }
