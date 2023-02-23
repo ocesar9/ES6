ec866cc356accd22e0cd59ec87f3dc145756fd1b
ocesar9 <juliocesaoguimaraes@gmail.com>
2023-02-22 21:52:05 -0300

    Refatorar Tabnav

--- projeto-final/animais-fantasticos/js/modules/tabnav.js
+++ projeto-final/animais-fantasticos/js/modules/tabnav.js
@@ -1,22 +1,31 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll("[data-tab='tabmenu'] li");
-  const tabContent = document.querySelectorAll("[data-tab='content'] section");
-
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
-    });
-    const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add('ativo', direcao);
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
   }
 
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
-
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
-        activeTab(index);
-      });
+  // active the tab by the index of
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
     });
+    const direcao = this.tabContent[index].dataset.anime;
+    this.tabContent[index].classList.add(this.activeClass, direcao);
+  }
+
+  // add events to tabs
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => this.activeTab(index));
+    });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      // active first item
+      this.activeTab(0);
+      this.addTabNavEvent();
+    }
   }
 }
