25dec14d97e6b3ae4df12d26251a26bfe817b4e6
ocesar9 <juliocesaoguimaraes@gmail.com>
2023-02-22 21:33:17 -0300

    Refatorar Accordion

--- projeto-final/animais-fantasticos/js/modules/scroll-suave.js
+++ projeto-final/animais-fantasticos/js/modules/scroll-suave.js
@@ -1,12 +1,8 @@
 export default class ScrollSuave {
   constructor(links, options) {
     this.linksInternos = document.querySelectorAll(links);
-
     if (options === undefined) {
-      this.options = {
-        behavior: 'smooth',
-        block: 'start',
-      };
+      this.options = { behavior: 'smooth', block: 'start' };
     } else {
       this.options = options;
     }
@@ -28,7 +24,7 @@ export default class ScrollSuave {
   }
 
   init() {
-    if (this.links.length) {
+    if (this.linksInternos.length) {
       this.addLinkEvent();
     }
     return this;
