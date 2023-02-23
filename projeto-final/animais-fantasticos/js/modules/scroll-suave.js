f105766777490e246c91c3d35aa2fa3e4d64f28b
ocesar9 <juliocesaoguimaraes@gmail.com>
2023-02-22 21:01:46 -0300

    Refatorar Scroll Suave

---
+++ projeto-final/animais-fantasticos/js/modules/scroll-suave.js
@@ -0,0 +1,44 @@
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+
+    if (options === undefined) {
+      this.options = {
+        behavior: 'smooth',
+        block: 'start',
+      };
+    } else {
+      this.options = options;
+    }
+
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.links.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
+}
+
+// Formal alternativa
+// const topo = section.offsetTop
+// console.log(href,section,topo);
+// window.scrollTo({
+//   top: topo,
+//   behavior: "smooth",
+// });
