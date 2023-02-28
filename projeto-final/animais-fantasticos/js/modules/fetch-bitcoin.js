f19b8065767139aeb14b5b9fe83a559c060b2023
ocesar9 <juliocesaoguimaraes@gmail.com>
2023-02-28 09:00:29 -0300

    Refatorar Fetch Bitcoin

--- projeto-final/animais-fantasticos/js/modules/fetch-bitcoin.js
+++ projeto-final/animais-fantasticos/js/modules/fetch-bitcoin.js
@@ -1,11 +1,12 @@
-export default function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker')
+export default function fetchBitcoin(url, target) {
+  fetch(url)
     .then((response) => response.json())
     .then((bitcoin) => {
-      const btcPreco = document.querySelector('.btc-preco');
-      btcPreco.innerText = (100 / (bitcoin.BRL.sell)).toFixed(4);
+      const btcPreco = document.querySelector(target);
+      btcPreco.innerText = (100 / bitcoin.BRL.sell).toFixed(4);
       console.log(bitcoin.BRL.sell);
-    }).catch((error) => console.log((error)));
+    })
+    .catch((error) => console.log(error));
 }
 
 // https://bloackchain.info/ticker
--- projeto-final/animais-fantasticos/js/script.js
+++ projeto-final/animais-fantasticos/js/script.js
@@ -6,7 +6,7 @@ import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import fetchBitcoin from './modules/fetch-bitcoin.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import fetchAnimais from './modules/fetch-animais.js';
 
@@ -35,7 +35,7 @@ tooltip.init();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchBitcoin();
 initAnimacaoScroll();
 
 fetchAnimais('./animaisapi.json', '.numeros-grid');
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
