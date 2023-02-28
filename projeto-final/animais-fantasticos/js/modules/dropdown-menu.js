54435f376876b77cf5efaf3c73784257f575e501
ocesar9 <juliocesaoguimaraes@gmail.com>
2023-02-28 11:14:37 -0300

    Refatorar Dropdown Menu

--- projeto-final/animais-fantasticos/js/modules/dropdown-menu.js
+++ projeto-final/animais-fantasticos/js/modules/dropdown-menu.js
@@ -1,20 +1,40 @@
 import outsideClick from './outsideclick.js';
 
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-  function handleClick(event) {
+export default class DropdownMenu {
+  constructor(dropdownMenus, events) {
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus);
+
+    // define the touchstart and click by default if unset
+    if (events === undefined) this.events = ['touchstart', 'click'];
+    else this.events = events;
+
+    this.activeClass = 'active';
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
+
+  // Active dropdown menu e add listeners
+  activeDropdownMenu(event) {
     event.preventDefault();
-    this.classList.toggle('active');
-    outsideClick(this, ['touchstart', 'click'], () => {
-      this.classList.remove('active');
+    const element = event.currentTarget;
+    element.classList.toggle(this.activeClass);
+    outsideClick(element.currentTarget, this.events, () => {
+      element.classList.remove(this.activeClass);
     });
   }
 
-  if (dropdownMenus) {
-    dropdownMenus.forEach((menu) => {
+  // add events to dropdown menu
+  addDropdownMenuEvent() {
+    this.dropdownMenus.forEach((menu) => {
       ['touchstart', 'click'].forEach((userEvent) => {
-        menu.addEventListener(userEvent, handleClick);
+        menu.addEventListener(userEvent, this.activeDropdownMenu);
       });
     });
+  }
+
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addDropdownMenuEvent();
+    }
+    return this;
   }
 }
